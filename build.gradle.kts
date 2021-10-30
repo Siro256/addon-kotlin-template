edcc73c44f3b8fba7b30f9e064b9a9033413ac84
Siro_256 <siro@siro256.dev>
2021-10-30 15:10:10 +0900

    Update Kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import net.minecraftforge.gradle.userdev.UserDevExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.30"
+    kotlin("jvm") version "1.5.31"
 }
 
 buildscript {
